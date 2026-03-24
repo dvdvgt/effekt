e1f7f241fa6b476c003281542c46ee35d3dd37ad
dvdvgt <40773635+dvdvgt@users.noreply.github.com>
2026-03-24 14:26:26 +0100

    show output for debugging

--- .github/scripts/benchmark.sh
+++ .github/scripts/benchmark.sh
@@ -69,6 +69,7 @@ for benchmark in "${benchmarks[@]}"; do
   filename=${path##*/}
     hyperfine \
       --warmup 1 \
+      --show-output \
       --export-markdown "$out_dir/$filename.md" \
       --export-csv "$out_dir/$filename.csv" \
       --export-json "$out_dir/$filename.json" \
